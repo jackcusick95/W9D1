27f2c1ad803bf2fe1479ace1e46f9438b7267572
jessicaUP <uphoff.jessica@gmail.com>
2021-04-19 17:33:24 -0400

    Arguments

--- arguments.js
+++ arguments.js
@@ -41,3 +41,49 @@ class Dog {
 
 const markov = new Cat("Markov");
 const pavlov = new Dog("Pavlov");
+
+
+function curriedSum(numArgs) {
+  let numbers = [];
+  let sum = 0;
+
+  return function _curriedSum(num) {
+    numbers.push(num);
+    sum += num;
+    if (numbers.length === numArgs) {
+      return console.log(sum);
+    }
+    return _curriedSum;
+  };
+}
+
+
+// function curriedSum(numArgs) {
+//   let numbers = [];
+
+//   return function _curriedSum(num) {
+//     numbers.push(num);
+//     if (numbers.length === numArgs) {
+//       numbers.sum
+//       return console.log(sum);
+//     };
+// };
+// }
+
+
+Function.prototype.waitArgs = function (context, numArgs){
+  let funArgs = [];
+  let func = this;
+  // count = 0
+
+  return function (arg) {
+    if (numArgs === funArgs.length+1) {
+      funArgs.push(arg);
+      return func.apply(context, funArgs)
+    } else {
+      funArgs.push(arg);
+      // count += 1;
+    };
+  };
+
+}
